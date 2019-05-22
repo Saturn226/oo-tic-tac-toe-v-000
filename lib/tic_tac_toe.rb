95a0752207ee08a7c86358c026b72843f4dc6522
Ashley Muller <serenehopes@gmail.com>
2019-05-22 06:14:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,12 @@ WIN_COMBINATIONS = [
     @board = Array.new(" ", 9)
   end
   
-  def display_
+ def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts '-----------'
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts '-----------'
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
   
 end
